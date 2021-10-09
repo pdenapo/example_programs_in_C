ffa18615606afee9ec1c23c891479c26ce689f4d
Pablo De Nápoli <pdenapo@gmail.com>
2021-10-08 23:59:21 -0300

    Fix compilation of Duktape-console.

--- duktape-console/duktape-console.c
+++ duktape-console/duktape-console.c
@@ -3,29 +3,32 @@
 #include <stdio.h>
 #include "duktape.h"
 #include "duk_print_alert.h"
-#include "readline.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   duk_context *ctx = duk_create_heap_default();
   duk_print_alert_init(ctx, 0 /*flags*/);
   printf("Duktape console\n");
   while (1)
   {
-    char* line = readline (">");
-	  if (!line)
-     	break; 	
-   	int ret = duk_peval_string(ctx, line);
+    char *line = readline(">");
+    if (!line)
+      break;
+    int ret = duk_peval_string(ctx, line);
 
-        if (ret != 0) {
-            printf("Error: %s \n ",duk_to_string(ctx, -1));
-        }
-        else {
-            if (!duk_is_undefined(ctx, -1))
-                printf("%s \n",duk_to_string(ctx, -1));
-        }
-        
-        duk_pop(ctx);
-    
+    if (ret != 0)
+    {
+      printf("Error: %s \n ", duk_to_string(ctx, -1));
+    }
+    else
+    {
+      if (!duk_is_undefined(ctx, -1))
+        printf("%s \n", duk_to_string(ctx, -1));
+    }
+
+    duk_pop(ctx);
   }
   duk_destroy_heap(ctx);
   return 0;
