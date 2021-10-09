8b204cf6bd627ab501638a85a49da0c041b6915c
Pablo De Nápoli <pdenapo@gmail.com>
2021-10-09 00:19:58 -0300

    Fix compilation of Linnoise-minimal

--- linenoise-minimal/linenoise-minimal.c
+++ linenoise-minimal/linenoise-minimal.c
@@ -1,18 +1,19 @@
 /* test.c */
 #include <stdio.h>
 #include <stdlib.h>
-#include <linenoise.h>
+#include "linenoise.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   printf("Linenoise console\n");
   while (1)
   {
-    char* line = linenoise (">");
-	  if (!line)
-     	break; 	
+    char *line = linenoise(">");
+    if (!line)
+      break;
     linenoiseHistoryAdd(line);
-    printf("%s \n",line);
-    free(line);  
+    printf("%s \n", line);
+    free(line);
   }
   return 0;
 }
